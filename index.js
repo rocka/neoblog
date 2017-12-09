2ff81811dbde94080ac40a31ab477bafaed5a068
rocka <i@rocka.me>
2017-12-09 22:19:46 +0800

    bin/cli: handle keypress * `^R` : reload config * `^C` : stop server and exit

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 'use strict';
 
-const path = require('path');
+const Path = require('path');
+const ReadLine = require('readline');
 
-const server = require('./src/server');
-const configPath = path.join(process.cwd(), 'config.js');
+ReadLine.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
 
-new server(configPath).start();
+const Server = require('./src/server');
+const configPath = Path.join(process.cwd(), 'config.js');
+
+const neoblog = new Server(configPath);
+
+neoblog.start();
+
+process.stdin.on('keypress', (ch, key) => {
+    if (key && key.ctrl) {
+        switch (key.name) {
+            case 'r':
+                console.log('\nCtrl-R pressed, reloading config...');
+                neoblog.reload();
+                break;
+            case 'c':
+                console.log('\nCtrl-C pressed, exiting...');
+                neoblog.stop().then(() => {
+                    process.exit(0);
+                });
+                break;
+            default:
+                break;
+        }
+    }
+});

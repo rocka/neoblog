9c56dd28a9b3ad4e83babd5ad05fe2af3ed06ba0
rocka <i@rocka.me>
2017-12-11 18:23:03 +0800

    built-in: markdown: add support for gfm task list
    - fix crash when there is no language specificed in code block

--- built-in/plugin/markdown.js
+++ built-in/plugin/markdown.js
@@ -2,31 +2,39 @@
 
 const Marked = require('marked');
 const HLJS = require('highlight.js');
+const EscapeHTML = require('escape-html');
 
-const renderer = new Marked.Renderer({ breaks: true });
+const renderer = new Marked.Renderer();
+
+renderer.listitem = function (text) {
+    const html = text
+        .replace(/^\s*\[x\]\s*/g, '<i class="material-icons" aria-hidden="true">check_box</i>')
+        .replace(/^\s*\[ \]\s*/g, '<i class="material-icons" aria-hidden="true">check_box_outline_blank</i>');
+    return `<li>${html}</li>`;
+};
 
 renderer.image = function (href, title, text) {
     return `<figure><img src="${href}"><figcaption>${text}</figcaption></figure>`;
 };
 
 renderer.heading = function (text, level) {
-    const escapedText = text.replace(/[^\w]+/g, '-');
+    const escaped = EscapeHTML(text);
     // <h1><a name="heading" href="#heading" class="anchor"><span class="header-link">heading</span></a></h1>
-    return `<h${level}><a name="${escapedText}" href="#${escapedText}" class="anchor"><span class="header-link">${text}</span></a></h${level}>`;
+    return `<h${level}><a name="${escaped}" href="#${escaped}" class="anchor"><span class="header-link">${text}</span></a></h${level}>`;
 };
 
 renderer.code = function (code, lang) {
     /** @type {hljs.IHighlightResultBase} */
     let result = {};
-    if (lang) {
+    if (lang && HLJS.getLanguage(lang)) {
         result = HLJS.highlight(lang, code);
     } else {
-         result = HLJS.highlightAuto(code);
+        result = HLJS.highlightAuto(code);
     }
     return `<pre class="hljs"><code class="lang-${result.language}" data-language="${result.language}">${result.value}</code></pre>`;
 };
 
-Marked.setOptions({ renderer: renderer });
+Marked.setOptions({ renderer });
 
 function install(server) {
     server.parser.on('md', (src, resolve) => {
